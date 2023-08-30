20a0d6eb8d4be24ef068d758a20de57204a47987
Melrok <martin.blascak86@gmail.com>
2023-08-30 14:39:49 +0200

    init lucka page

---
+++ src/views/LuckaView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="lucka-page-wrapper">
+        <h1>Lucka</h1>
+
+    </div>
+</template>
+
+
+<script lang="ts">
+export default {
+    setup() {
+        const over = false;
+    },
+}
+</script>
+
+
+<style lang="sass" scoped>
+
+
+
+</style>
