bb1dd93a3d2c2c91c70c8c8f25188c0345b97398
Melrok <martin.blascak86@gmail.com>
2023-08-30 15:21:58 +0200

    space size

--- src/views/LuckaView.vue
+++ src/views/LuckaView.vue
@@ -1,22 +1,18 @@
 <template>
-    <div class="lucka-page-wrapper">
-        <h1>Lucka</h1>
-
-    </div>
+  <div class="lucka-page-wrapper">
+    <h1>Lucka</h1>
+  </div>
 </template>
 
 
 <script lang="ts">
 export default {
-    setup() {
-        const over = false;
-    },
-}
+  setup() {
+    const over = false;
+  },
+};
 </script>
 
 
 <style lang="sass" scoped>
-
-
-
 </style>
