494115f71c1cef2af57d5e8f8f8c0a7da9586307
Rysana <lucia.kovacova.86@gmail.com>
2023-09-14 16:40:28 +0200

    lucka

--- src/views/LuckaView.vue
+++ src/views/LuckaView.vue
@@ -1,17 +1,37 @@
 <template>
-  <div class="lucka-page-wrapper">
-    <h1>Lucka</h1>
-		<div></div>
+  <div class="app">
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="changeName('Tarffffffa')">Change name</button>
+    <button @click="changeAge(250000)">Change age</button>
   </div>
 </template>
 
 
 <script lang="ts">
-export default {
-  setup() {
-    const over = false;
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "LuckaView",
+  components: {},
+ setup() {
+  const name = ref('Link')
+  const age = ref<number | string>(35)
+
+  return {name, age}
+ },
+
+  methods: {
+    changeName(name: string) {
+      this.name = name
+      return name
+    },
+
+    changeAge(age: string | number) {
+      this.age = age
+      return age
+    },
   },
-};
+});
 </script>
 
 
