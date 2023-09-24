aa20cc92dd1d657b26853dc84f7154929ff378cc
Melrok <martin.blascak86@gmail.com>
2023-09-24 11:06:30 +0200

    change of spaces from two to 4

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,29 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import LuckaView from '../views/LuckaView.vue'
-import AboutView from '../views/AboutView.vue'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import LuckaView from '../views/LuckaView.vue';
+import AboutView from '../views/AboutView.vue';
+import HomeView from '../views/HomeView.vue';
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: AboutView
-  },
-  {
-    path: '/lucka',
-    name: 'lucka',
-    component: LuckaView
-  }
-]
+    {
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/about',
+        name: 'about',
+        component: AboutView
+    },
+    {
+        path: '/lucka',
+        name: 'lucka',
+        component: LuckaView
+    }
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+});
 
-export default router
+export default router;
